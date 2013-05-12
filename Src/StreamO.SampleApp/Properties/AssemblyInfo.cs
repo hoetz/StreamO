0affd06ed1a244870ec32bca2839b01374eef443
Florian Hötzinger <florian@hoetzinger.de>
2013-05-12 14:53:01 +0200

    Restructured Solution

--- Src/StreamO.SampleApp/Properties/AssemblyInfo.cs
+++ Src/StreamO.SampleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StreamO")]
+[assembly: AssemblyTitle("StreamO.SampleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StreamO")]
+[assembly: AssemblyProduct("StreamO.SampleApp")]
 [assembly: AssemblyCopyright("Copyright © Florian Hötzinger  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c506ee7-067f-48d2-8c77-99f8025c7dc0")]
+[assembly: Guid("ee4a4fa2-6843-479c-9998-92dbb303fcb5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
